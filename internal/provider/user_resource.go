ba7bf57efdf23f28469da8710e10ad622b7c89e9
Michael Whitaker <mcwhitaker@fastmail.com>
2024-04-19 08:40:32 -0500

    Intermediary commit of rough resource read

--- internal/provider/user_resource.go
+++ internal/provider/user_resource.go
@@ -2,11 +2,14 @@ package provider
 
 import (
     "context"
+    "time"
+    "fmt"
 
     "go.mongodb.org/mongo-driver/bson"
     "go.mongodb.org/mongo-driver/mongo"
     "github.com/hashicorp/terraform-plugin-framework/resource"
     "github.com/hashicorp/terraform-plugin-framework/resource/schema"
+    "github.com/hashicorp/terraform-plugin-framework/types"
 )
 
 var (
@@ -34,6 +37,28 @@ type userRoleModel struct {
   Role types.String `tfsdk:"role"`
 }
 
+type commandResponse struct {
+  OK            int       `bson:"ok"`
+  OperationTime time.Time `bson:"operationTime"`
+}
+
+type dbUser struct {
+  Id         string `bson:"_id"`
+  User       string `bson:"user"`
+  Db         string `bson:"db"`
+  Roles      []dbRole `bson:"roles"`
+}
+
+type dbRole struct {
+	Role string `bson:"role"`
+	Db   string `bson:"db"`
+}
+
+type readResponse struct {
+  commandResponse `bson:",inline"`
+  Users []dbUser `bson:"users"`
+}
+
 func (r *userResource) Configure(_ context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
     if req.ProviderData == nil {
         return
@@ -59,7 +84,7 @@ func (r *userResource) Metadata(_ context.Context, req resource.MetadataRequest,
 
 func (r *userResource) Schema(_ context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
     resp.Schema = schema.Schema{
-      Attributes: map[string].schema.Attribute{
+      Attributes: map[string]schema.Attribute{
         "db": schema.StringAttribute{
           Required: true,
         },
@@ -83,52 +108,51 @@ func (r *userResource) Schema(_ context.Context, _ resource.SchemaRequest, resp 
             },
           },
         },
-      }
+      },
     }
 }
 
 func (r *userResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
     var plan userResourceModel
     diags := req.Plan.Get(ctx, &plan)
-    resp.Diagnostics.Append(diag...)
+    resp.Diagnostics.Append(diags...)
     if resp.Diagnostics.HasError() {
       return
     }
 
     var roles []bson.M
     for _, role := range plan.Roles {
-      roles = append(roles, bson.M{{"role": role.Role, "db": role.Db}})
+      roles = append(roles, bson.M {"role": role.Role.ValueString(), "db": role.Db.ValueString()})
     }
 
-    userCreateCommand := bson.D {{"createUser", plan.Name}, {"pwd", plan.Password}, {"roles", roles}}
+    userCreateCommand := bson.D {{"createUser", plan.User.ValueString()}, {"pwd", plan.Password.ValueString()}, {"roles", roles}}
 
-    mongoResult := client.Database(plan.Db).RunCommand(context, userCreateCommand)
+    mongoResult := r.client.Database(plan.Db.ValueString()).RunCommand(ctx, userCreateCommand)
     if mongoResult.Err() != nil {
       resp.Diagnostics.AddError(
           "Error creating user",
-          "Could not create user, unexpected error: " + mongoReuslt.Err(),
+          "Could not create user, unexpected error: " + mongoResult.Err().Error(),
       )
       return
     }
 
-    plan.ID = types.StringValue(str
+    var response commandResponse
+    err := mongoResult.Decode(&response)
+    if err != nil {
+      resp.Diagnostics.AddError(
+        "Error creating user",
+        "Could not create user, unexpected error: " + err.Error(),
+      )
 
-    // Map response body to schema and populate Computed attribute values
-    plan.ID = types.StringValue(strconv.Itoa(order.ID))
-    for orderItemIndex, orderItem := range order.Items {
-        plan.Items[orderItemIndex] = orderItemModel{
-            Coffee: orderItemCoffeeModel{
-                ID:          types.Int64Value(int64(orderItem.Coffee.ID)),
-                Name:        types.StringValue(orderItem.Coffee.Name),
-                Teaser:      types.StringValue(orderItem.Coffee.Teaser),
-                Description: types.StringValue(orderItem.Coffee.Description),
-                Price:       types.Float64Value(orderItem.Coffee.Price),
-                Image:       types.StringValue(orderItem.Coffee.Image),
-            },
-            Quantity: types.Int64Value(int64(orderItem.Quantity)),
-        }
+      return
     }
-    plan.LastUpdated = types.StringValue(time.Now().Format(time.RFC850))
+
+    if response.OK != 1 {
+      resp.Diagnostics.AddError(
+        "Error creating user",
+        fmt.Sprintf("Could not create user, unexpected error returned from MongoDB: %d", response.OK))
+      return
+    }
 
     // Set state to fully populated data
     diags = resp.State.Set(ctx, plan)
@@ -138,10 +162,52 @@ func (r *userResource) Create(ctx context.Context, req resource.CreateRequest, r
     }
 }
 
-
-
-
 func (r *userResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+  var state userResourceModel
+  diags := req.State.Get(ctx, &state)
+  resp.Diagnostics.Append(diags...)
+  if resp.Diagnostics.HasError() {
+    return
+  }
+
+  var usersInfo readResponse
+  cmd := bson.D{{Key: "usersInfo", Value: bson.M{
+      "user": state.User.ValueString(),
+      "db":   state.Db.ValueString(),
+  }}}
+
+  err := r.client.Database(state.Db.ValueString()).RunCommand(ctx, cmd).Decode(&usersInfo)
+  if err != nil {
+    resp.Diagnostics.AddError(
+        "Error reading user from MongoDb",
+        "Could not retrieve user <" + state.User.ValueString() + "> " + err.Error())
+  }
+
+
+  users := usersInfo.Users
+  if len(users) == 0 {
+    resp.Diagnostics.AddError(
+        "Error reading user from MongoDb",
+        "Could not retrieve user <" + state.User.ValueString() + "> " + err.Error())
+  }
+
+  user := users[0]
+  state.User = types.StringValue(user.User)
+  state.Db = types.StringValue(user.Db)
+
+  state.Roles = []userRoleModel{}
+  for _, item := range user.Roles {
+    state.Roles = append(state.Roles, userRoleModel {
+      Db: types.StringValue(item.Db),
+      Role: types.StringValue(item.Role),
+    })
+  }
+
+  diags = resp.State.Set(ctx, &state)
+  resp.Diagnostics.Append(diags...)
+  if (resp.Diagnostics.HasError()) {
+    return
+  }
 }
 
 func (r *userResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
